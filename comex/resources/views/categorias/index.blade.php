c604336e2ead787656d58e5efe05b25015ceeaa3
Anderson Andrade <139899277+AndersonCCandrade@users.noreply.github.com>
2023-11-20 23:39:21 -0300

    Tarefa 8 - Estilização do projeto com Bootstrap
    Certifique-se que você tem instalado o NodeJS 18 ou posterior.
    
    Siga os passos da aula para configurar o Laravel Mix. Não utilize o Vite.
    
    Configure o Boostrap e o utilize para melhorar o estilo das páginas!

--- comex/resources/views/categorias/index.blade.php
+++ comex/resources/views/categorias/index.blade.php
@@ -2,27 +2,28 @@
     <br>
 
     @isset($mensagemSucesso)
-        <div>
+        <div class="alert alert-success">
             {{$mensagemSucesso}}
         </div>
         <br>
     @endisset
 
-    <a href="{{route('categorias.create')}}">Adicionar</a>
-    <br>
-    <ul >
+    <a href="{{route('categorias.create')}}" class="btn btn-dark mb-2">Adicionar</a>
+    <br><br>
+    <ul class="list-group">
         @foreach ($categoria as $list)
-                <li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
                     {{$list->nome}}
                     <span>
                         <form action="{{ route('categorias.destroy', $list->id) }}" method="post">
+
+                            <a href="{{ route('categorias.edit', $list->id) }}" class="btn btn-primary btn-sm">
+                               Editar
+                            </a>
                             @csrf
                             @method('DELETE')
-                            <button >Remover</button>
-                        </form>
-                        <form action="{{ route('categorias.edit', $list->id) }}" method="post">
-                            @method('GET')
-                            <button >Editar</button>
+                            <button class="btn btn-danger btn-sm">Remover</button>
+
                         </form>
                     </span>
 
--- comex/resources/views/components/categorias/form.blade.php
+++ comex/resources/views/components/categorias/form.blade.php
@@ -3,16 +3,19 @@
     @isset($nome)
         @method('PUT')
     @endisset
-    <div>
-        <label for="nome" >Nome:</label>
+    <br>
+    <div class = "mb-3">
+        <label for="nome" class="form-label">Nome:</label>
         <input type="text"
+               autofocus
                id="nome"
                name="nome"
+               class="form-control"
                @isset($nome) value="{{ $nome }}" @endisset
         >
     </div>
-    <br>
-    <button type="submit" >
+
+    <button type="submit" class="btn btn-primary">
         @isset($nome)
             Atualizar
         @else
